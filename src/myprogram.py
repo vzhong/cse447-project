d4b936216543781b053d9b118372d03f90d37743
Jeffery Tian <jefftian@uw.edu>
2021-02-16 23:35:52 -0800

    loading data works

--- src/myprogram.py
+++ src/myprogram.py
@@ -2,6 +2,7 @@
 import os
 import string
 import random
+import pickle
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
@@ -9,22 +10,27 @@ class MyModel:
     """
     This is a starter model to get you started. Feel free to modify this file.
     """
-
+    n = 3
+    # lang -> ngrams -> list[unigram, bigram ...], each model is a dictionary {prefix:probablity}
+    lang_to_ngrams = {}
+    start_char = '¢'
+    stop_char = '£'
     @classmethod
     def load_training_data(cls):
-        # your code here
-        # this particular model doesn't train
-        return []
+        # trainPath = r'../shortTranslations/AllTrain'
+        trainPath = r'../data/VeryShort'
+        files = os.listdir(trainPath)
+        out = {}
+        for f in files:
+            lang_code = f[:f.find('train')]
+            f_ = open(os.path.join(trainPath, f), "r", encoding='utf-16')
+            out[lang_code] = f_.read().split("\n")
+        return out
 
     @classmethod
     def load_test_data(cls, fname):
-        # your code here
-        data = []
-        with open(fname) as f:
-            for line in f:
-                inp = line[:-1]  # the last character is a newline
-                data.append(inp)
-        return data
+        f_ = open(fname, "r")
+        return f_.read().split("\n")
 
     @classmethod
     def write_pred(cls, preds, fname):
@@ -33,8 +39,60 @@ class MyModel:
                 f.write('{}\n'.format(p))
 
     def run_train(self, data, work_dir):
-        # your code here
-        pass
+        for lang in data:
+            self.lang_to_ngrams[lang] = []
+            for i in range(len(data[lang])):
+                data[lang][i] = (self.n - 1) * self.start_char + data[lang][i] + self.stop_char
+
+            # constuct frequency maps and total counts
+            total_counts = {}
+            for i in range(self.n):
+                self.lang_to_ngrams[lang].append({})
+                total_counts[i] = {}
+            # unigram is a special case
+            total_counts[0] = 0
+            # fill in frequency maps
+            for line in data[lang]:
+                for i in range(len(line)):
+                    for model_index in range(self.n):
+                        remove_starts = self.n - 1 - model_index
+                        index = i + remove_starts
+                        if(index + model_index >= len(line)):
+                            continue
+                        # unigram is special case of char -> freq
+                        # other n-grams are prefix -> char-> freq
+                        if model_index == 0:
+                            char = line[index]
+                            if char in self.lang_to_ngrams[lang][model_index]:
+                                self.lang_to_ngrams[lang][model_index][char] += 1
+                            else:
+                                self.lang_to_ngrams[lang][model_index][char] = 1
+                            total_counts[model_index] += 1
+                        else:
+                            prefix = line[index : index + model_index]
+                            char = line[index + model_index]
+                            if prefix in self.lang_to_ngrams[lang][model_index]:
+                                if char in self.lang_to_ngrams[lang][model_index][prefix]:
+                                    self.lang_to_ngrams[lang][model_index][prefix][char] += 1
+                                else:
+                                    self.lang_to_ngrams[lang][model_index][prefix][char] = 1
+                            else:
+                                self.lang_to_ngrams[lang][model_index][prefix] = {}
+                                self.lang_to_ngrams[lang][model_index][prefix][char] = 1
+                            if prefix in total_counts[model_index]:
+                                total_counts[model_index][prefix] += 1
+                            else:
+                                total_counts[model_index][prefix] = 1
+            # convert to probabilities
+            for model_index in range(len(self.lang_to_ngrams[lang])):
+                # unigram is a special case
+                if model_index == 0:
+                    for char in self.lang_to_ngrams[lang][model_index]:
+                        self.lang_to_ngrams[lang][model_index][char] /= total_counts[model_index]
+                else:
+                    for prefix in self.lang_to_ngrams[lang][model_index]:
+                        for char in self.lang_to_ngrams[lang][model_index][prefix]:
+                            self.lang_to_ngrams[lang][model_index][prefix][char] /= total_counts[model_index][prefix]
 
     def run_pred(self, data):
         # your code here
@@ -49,24 +107,29 @@ class MyModel:
     def save(self, work_dir):
         # your code here
         # this particular model has nothing to save, but for demonstration purposes we will save a blank file
-        with open(os.path.join(work_dir, 'model.checkpoint'), 'wt') as f:
-            f.write('dummy save')
+        with open(os.path.join(work_dir, 'model.checkpoint'), 'wb') as f:
+            pickle.dump(self.lang_to_ngrams, f)
 
-    @classmethod
+    @ classmethod
     def load(cls, work_dir):
         # your code here
         # this particular model has nothing to load, but for demonstration purposes we will load a blank file
-        with open(os.path.join(work_dir, 'model.checkpoint')) as f:
-            dummy_save = f.read()
-        return MyModel()
+        with open(os.path.join(work_dir, 'model.checkpoint'), 'rb') as f:
+            lang_to_ngrams = pickle.load(f)
+        model = MyModel()
+        model.lang_to_ngrams = lang_to_ngrams
+        return model
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('mode', choices=('train', 'test'), help='what to run')
+    parser.add_argument('mode', choices=(
+        'train', 'test'), help='what to run')
     parser.add_argument('--work_dir', help='where to save', default='work')
-    parser.add_argument('--test_data', help='path to test data', default='example/input.txt')
-    parser.add_argument('--test_output', help='path to write test predictions', default='pred.txt')
+    parser.add_argument('--test_data', help='path to test data',
+                        default='example/input.txt')
+    parser.add_argument(
+        '--test_output', help='path to write test predictions', default='pred.txt')
     args = parser.parse_args()
 
     random.seed(0)
@@ -81,6 +144,7 @@ if __name__ == '__main__':
         train_data = MyModel.load_training_data()
         print('Training')
         model.run_train(train_data, args.work_dir)
+        print(model.lang_to_ngrams)
         print('Saving model')
         model.save(args.work_dir)
     elif args.mode == 'test':
@@ -91,7 +155,8 @@ if __name__ == '__main__':
         print('Making predictions')
         pred = model.run_pred(test_data)
         print('Writing predictions to {}'.format(args.test_output))
-        assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(len(test_data), len(pred))
+        assert len(pred) == len(test_data), 'Expected {} predictions but got {}'.format(
+            len(test_data), len(pred))
         model.write_pred(pred, args.test_output)
     else:
         raise NotImplementedError('Unknown mode {}'.format(args.mode))
